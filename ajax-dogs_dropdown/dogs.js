fe74e55388c636c6dd6e2be82b61c5432ec55c42
Rafael Plinio <racpsjcsp@gmail.com>
2019-10-03 15:34:21 -0300

    Ajax -Dropdown Menu & Pictures
    Loading spinner
    Roll button (reload a new image)

--- ajax-dogs_dropdown/dogs.js
+++ ajax-dogs_dropdown/dogs.js
@@ -1,6 +1,5 @@
 //Get a random dog picture
 /* const dogs_url = 'https://dog.ceo/api/breeds/image/random';
-
 function addDoggo() {
   fetch(dogs_url)
     .then(function(response) {
@@ -18,10 +17,12 @@ function addDoggo() {
 //Get a random dog based on the chosen breed
 const breed_url = 'https://dog.ceo/api/breeds/list/all';
 const select = document.querySelector('.breeds');
+const button = document.querySelector('.roll');
+
 
 fetch(breed_url)
-  //spinner start
-  
+  //spinner should start here
+
   .then(function(response) {
     return response.json();
   })
@@ -40,30 +41,41 @@ fetch(breed_url)
   })
 
   select.addEventListener("change", function(event) {
-    let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`
-    //spinner end
+    let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+
+    //spinner should end here
 
     getDoggo(url);
- 
-  });
+
+    //roll button reloads a new random pic of the chosen breed
+    button.addEventListener('click', function(event) {
+      getDoggo(url);
+    });
+
+    });
 
 
   const img = document.querySelector('.dog-img');
-
+  const spinner = document.querySelector('.spinner');
+  
   function getDoggo(url) {
-    fetch(url)
+    //spinner starts
+    spinner.classList.add('show');
+    img.classList.remove('show');
+
+      fetch(url)
       .then(function(res) {
         return res.json();
       })
       .then(function(data) {
         console.log(data);
         img.src = data.message;
-      })
-      
+
+        //spinner ends
+      });      
   }
-
   
-
-
-  
-
+  img.addEventListener('load', function() {
+    spinner.classList.remove('show');
+    img.classList.add('show');
+  })
